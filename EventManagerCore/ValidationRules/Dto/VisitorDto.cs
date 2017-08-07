ec4be6b8f4037662b3b2f0b25dc9f29b8f066ab0
Justus Bonz <jasdefer>
2017-08-07 11:35:13 +0200

    Added Data Transfer Objects (DTOs
    See issue #4

---
+++ EventManagerCore/ValidationRules/Dto/VisitorDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValidationRules.Dto
+{
+    public class VisitorDto
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string PasswordHash { get; set; }
+        public string Bio { get; set; }
+        IEnumerable<int> RegionIds { get; set; }
+    }
+}
